0e43bc33ef3e92e574b6a588168569f9d39d2dd4
Ernest <iendeavor.github.io@gmail.com>
2024-01-20 11:59:55 +0800

    feat: add command description (#52)

--- packages/cli/src/interpolate/command.ts
+++ packages/cli/src/interpolate/command.ts
@@ -5,8 +5,17 @@ import { readFileSync } from "fs";
 
 export default () => {
   return new Command("interpolate")
-    .option("--input-file-path <inputFilePath>")
-    .option("--output-file-path <outputFilePath>")
+    .description(
+      "perform template interpolation by substituting environment variables",
+    )
+    .option(
+      "--input-file-path <inputFilePath>",
+      "specify the input file to be loaded instead of being read from stdin",
+    )
+    .option(
+      "--output-file-path <outputFilePath>",
+      "specify the output file to be written instead of being piped to stdout.",
+    )
     .action(({ inputFilePath, outputFilePath }, { args }) => {
       const config = resolveConfig();
 
