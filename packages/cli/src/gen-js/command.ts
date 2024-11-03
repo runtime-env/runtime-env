5e5306105551c9def85228ee0d17d6a8eb442817
Ernest <22850435+soc221b@users.noreply.github.com>
2024-11-03 12:25:01 +0800

    feat: watch mode (#188)
    Co-authored-by: Ernest <>

--- packages/cli/src/gen-js/command.ts
+++ packages/cli/src/gen-js/command.ts
@@ -17,18 +17,28 @@ export default () => {
       "specify the output file to be written instead of being piped to stdout",
     )
     .action(async ({ outputFile, envFile }) => {
-      const { globalVariableName, schemaFile } = program.opts();
+      const { globalVariableName, schemaFile, watch } = program.opts();
 
-      const { output } = await act({
-        schemaFile,
-        globalVariableName,
-        envFiles: envFile,
-      });
+      await run();
+      if (watch) {
+        const chokidar = require("chokidar");
+        chokidar.watch([schemaFile, ...envFile]).on("change", async () => {
+          await run();
+        });
+      }
 
-      if (outputFile) {
-        writeFileSync(outputFile, output, "utf8");
-      } else {
-        console.log(output);
+      async function run() {
+        const { output } = await act({
+          schemaFile,
+          globalVariableName,
+          envFiles: envFile,
+        });
+
+        if (outputFile) {
+          writeFileSync(outputFile, output, "utf8");
+        } else {
+          console.log(output);
+        }
       }
     });
 };
