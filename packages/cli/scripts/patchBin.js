619e78b9ab8f2dde81c2bfe120d6a4c772702472
Ernest <soc221b.e@gmail.com>
2025-11-01 16:15:44 +0800

    fix: Error [ERR_UNKNOWN_BUILTIN_MODULE]: No such built-in module: serialize-javascript

--- packages/cli/scripts/patchBin.js
+++
@@ -1,7 +0,0 @@
-const { readFileSync, writeFileSync } = require("fs");
-
-const file = "bin/runtime-env.js";
-
-const oldContent = readFileSync(file, "utf-8");
-const newContent = `#!/usr/bin/env node\n'use strict';\n${oldContent}`;
-writeFileSync(file, newContent);
