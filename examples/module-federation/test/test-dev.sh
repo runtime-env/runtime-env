22c27ae700b0a70ba82903e62ddb0a8d4b3dd768
Ernest <soc221b.e@gmail.com>
2026-04-04 16:11:39 +0800

    Update test-dev.sh

--- examples/module-federation/test/test-dev.sh
+++ examples/module-federation/test/test-dev.sh
@@ -5,5 +5,5 @@ echo "VITE_MESSAGE=host-example" > ../host/.env
 
 npx start-server-and-test \
   "npm --prefix ../remote run dev" \
-  http://localhost:5174 \
+  http://localhost:3000 \
   "npx start-server-and-test 'npm --prefix ../host run dev' http://localhost:5173 'npx cypress run --config baseUrl=http://localhost:5173 --spec cypress/e2e/dev.cy.ts'"
