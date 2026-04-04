062c086e7e049ab9e9bd67b9e1e25f78ad86e44f
Ernest <soc221b.e@gmail.com>
2026-04-04 14:18:16 +0800

    Update cypress.config.ts

--- examples/module-federation/test/cypress.config.ts
+++ examples/module-federation/test/cypress.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    supportFile: false,
-    allowCypressEnv: false,
     video: false,
     screenshotOnRunFailure: true,
   },
