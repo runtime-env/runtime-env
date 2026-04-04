e27c36f6c4fe6f5c4d5677fcc9a5b7dc6d31658f
Ernest <soc221b.e@gmail.com>
2026-04-04 16:12:08 +0800

    Update test-preview.sh

--- examples/module-federation/test/test-preview.sh
+++ examples/module-federation/test/test-preview.sh
@@ -8,12 +8,12 @@ echo "VITE_MESSAGE=host-preview-initial" > ../host/.env
 
 npx start-server-and-test \
   "npm --prefix ../remote run preview" \
-  http://localhost:4174 \
+  http://localhost:3000 \
   "npx start-server-and-test 'npm --prefix ../host run preview' http://localhost:4173 'npx cypress run --config baseUrl=http://localhost:4173 --env EXPECTED_HOST=host-preview-initial,EXPECTED_REMOTE=host-preview-initial --spec cypress/e2e/preview.cy.ts'"
 
 echo "VITE_MESSAGE=host-preview-updated" > ../host/.env
 
 npx start-server-and-test \
   "npm --prefix ../remote run preview" \
-  http://localhost:4174 \
+  http://localhost:3000 \
   "npx start-server-and-test 'npm --prefix ../host run preview' http://localhost:4173 'npx cypress run --config baseUrl=http://localhost:4173 --env EXPECTED_HOST=host-preview-updated,EXPECTED_REMOTE=host-preview-updated --spec cypress/e2e/preview.cy.ts'"
